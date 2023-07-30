c09e3ada667cdede44090a2a8a93c8b80b25193b
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2023-07-30 10:52:49 -0300

    Update extract_from_pdf.py
    Adding libraries and functions

--- Generator Invoice/v2.1/Functions/extract_from_pdf.py
+++ Generator Invoice/v2.1/Functions/extract_from_pdf.py
@@ -1,24 +1,31 @@
 #Developed by Geeks for Geeks 
 #In future, I will create my version(objective of this: add knowledge)
 
+#Importing canvas from reportlab library 
+from reportlab.pdfgen import canvas
+
 #extract from pdf
 # importing required modules
 import PyPDF2
 
-# creating a pdf file object
-pdfFileObj = open('example.pdf', 'rb')
+def pdf_extractor():
+   # creating a pdf file object
+   pdfFileObj = open('example.pdf', 'rb')
 
-# creating a pdf reader object
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+   # creating a pdf reader object
+   pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-# printing number of pages in pdf file
-print(pdfReader.numPages)
+   # printing number of pages in pdf file
+   print(pdfReader.numPages)
 
-# creating a page object
-pageObj = pdfReader.getPage(0)
+   # creating a page object
+   pageObj = pdfReader.getPage(0)
 
-# extracting text from page
-print(pageObj.extractText())
+   # extracting text from page
+   print(pageObj.extractText())
 
-# closing the pdf file object
-pdfFileObj.close()
+   # closing the pdf file object
+   pdfFileObj.close()
+
+#call function
+pdf_extractor()
