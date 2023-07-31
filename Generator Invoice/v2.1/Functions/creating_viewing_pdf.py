f783012791a4fd0913ac44cb8d23a779da14c040
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2023-07-31 20:48:45 -0300

    Update creating_viewing_pdf.py
    adding code to merge projects release 1

--- Generator Invoice/v2.1/Functions/creating_viewing_pdf.py
+++ Generator Invoice/v2.1/Functions/creating_viewing_pdf.py
@@ -2,16 +2,37 @@
 #https://medium.com/@asttrodev/tutorial-gerando-pdf-simples-com-reportlab-e-python-6b67e3692bef
 
 #Extract_PDF Code-base developed by Geeks for Geeks 
+#https://
 #In future, I will create my version(objective of this: add knowledge)
 
 #Importing canvas from reportlab library 
 from reportlab.pdfgen import canvas
-
+# importing required modules
+import PyPDF2
 
 #declaring function
 def extract_PDF():
+   # creating a pdf file object
+   pdfFileObj = open('example.pdf', 'rb')
 
-    
+   # creating a pdf reader object
+   pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+   # printing number of pages in pdf file
+   print(pdfReader.numPages)
+
+   # creating a page object
+   pageObj = pdfReader.getPage(0)
+
+   # extracting text from page
+   print(pageObj.extractText())
+
+   # closing the pdf file object
+   pdfFileObj.close()
+
+#call function
+#extract_PDF()#test code
+
 #declaring function 
 def generate_PDF(list_Names):
     try:
