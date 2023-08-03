d4db165938d048c5d93ff3370e43473c47922362
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2023-08-03 19:58:26 -0300

    Update ui_toplevel.py

--- Generator Invoice/2.1.1/UI/ui_toplevel.py
+++ Generator Invoice/2.1.1/UI/ui_toplevel.py
@@ -1,1 +1,26 @@
+#Import necessaries libraries 
+import customtkinter as ctk
+import tkinter as tk
 
+#Specific import from libraries
+
+#Specific import from folders
+
+#Functions:
+#Geometry and Size
+
+#Buttons and Exit
+
+#Frame Class
+
+#Child Window Class 
+class Wx_Child(ctk.CTkTopLevel):
+   def __init__(parent):
+     super.__init__():
+     self.geometry("500x600")
+#Parent Window Class
+class Wx_Parent(ctk.CTk):
+   def __init__(self):
+      self.geometry("500x600")
+def wx_start():
+   print("Starting")
