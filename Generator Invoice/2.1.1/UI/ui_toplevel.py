d901b783056048792f95815ae6e38024def258e2
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2023-08-03 20:23:19 -0300

    Update ui_toplevel.py

--- Generator Invoice/2.1.1/UI/ui_toplevel.py
+++ Generator Invoice/2.1.1/UI/ui_toplevel.py
@@ -1,7 +1,8 @@
 #Import necessaries libraries 
 import customtkinter as ctk
 import tkinter as tk
-
+from tkinter import ttk
+      
 #Specific import from libraries
 
 #Specific import from folders
@@ -10,17 +11,33 @@ import tkinter as tk
 #Geometry and Size
 
 #Buttons and Exit
-
+def btn_open(ctk.CTk):
+   CTkButton(ctk.CTk,
+             text='Open a window',
+                command=lambda: wx_open(ctk.CTk)).pack(side='top',padx=20,pady=5,anchor='w')
+   
+def wx_open(ctk.CTk):
+        window = Wx_Child(CTk)
+        window.grab_set()
+def btn_close(ctk.CTkTopLevel):
+   CTkButton(CTkTopLevel,
+              text='Close',
+              command=CTkTopLevel.destroy).pack(side='top',padx=20,pady=5,anchor='w')
 #Frame Class
 
 #Child Window Class 
 class Wx_Child(ctk.CTkTopLevel):
-   def __init__(parent):
-     super.__init__():
-     self.geometry("500x600")
+   def __init__(self, parent):
+        super().__init__(parent)
+        self.geometry("500x600")
+        self.title("")
 #Parent Window Class
 class Wx_Parent(ctk.CTk):
    def __init__(self):
+      super().__init__()
       self.geometry("500x600")
+      self.title("")
 def wx_start():
    print("Starting")
+   wx = Wx_Parent()
+   wx.mainloop()
